8594098a7ada5ebe595e00cfaef4bf01df4a86db
wz <wzedan@gmail.com>
2024-04-14 21:48:41 +1000

    till data

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,33 +1,72 @@
 package com.example.application.views.list;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.example.application.data.Contact;
+import com.example.application.services.CrmService;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 
-@PageTitle("list")
+@PageTitle("Contacts")
 @Route(value = "")
 public class ListView extends VerticalLayout {
+    Grid<Contact> grid = new Grid<>(Contact.class);
+    TextField filter = new TextField();
 
-    public ListView() {
-        setSpacing(false);
-
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
-
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-
+    private final CrmService crmService;
+    ContactForm form;
+    public ListView(CrmService crmService) {
+        this.crmService = crmService;
+        addClassName("list-view");
         setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        configureGrid();
+        configureForm();
+        add(getToolbar(), getContent());
+        updateList();
     }
+
+    private Component getContent() {
+        HorizontalLayout content = new HorizontalLayout(grid ,form);
+        content.setFlexGrow(2, grid);
+        content.setFlexGrow(1 , form);
+        content.addClassName("content");
+        return content;
+    }
+
+    private void configureForm() {
+        form = new ContactForm(crmService.findAllCompanies(), crmService.findAllStatuses());
+        form.setWidth("25%");
+    }
+
+    private Component getToolbar() {
+        filter.setPlaceholder("Filter by name");
+        filter.setClearButtonVisible(true);
+        filter.setValueChangeMode(ValueChangeMode.LAZY);
+        filter.addValueChangeListener(e -> updateList());
+        Button addContactButton = new Button("Add contact");
+        var toolbar = new HorizontalLayout(filter, addContactButton);
+        toolbar.setClassName("toolbar");
+        return toolbar;
+    }
+
+    private void configureGrid() {
+        grid.addClassNames("contact-grid");
+        grid.setSizeFull();
+        grid.setColumns("firstName", "lastName", "email");
+        grid.addColumn(contact -> contact.getStatus().getName()).setHeader("Status");
+        grid.addColumn(contact -> contact.getCompany().getName()).setHeader("Company");
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+    }
+
+    private void updateList() {
+        grid.setItems(crmService.findAllContacts(filter.getValue()));
+    }
+
+
 
 }
